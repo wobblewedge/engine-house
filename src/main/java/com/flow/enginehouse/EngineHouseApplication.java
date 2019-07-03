3bcb5d533f626bec72224806e0ce07e3b054d9ea
wobblewedge <andobrown71@gmail.com>
2019-07-03 13:56:41 -0400

    confused

--- src/main/java/com/flow/enginehouse/EngineHouseApplication.java
+++ src/main/java/com/flow/enginehouse/EngineHouseApplication.java
@@ -2,7 +2,9 @@ package com.flow.enginehouse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @ComponentScan("com.flow.*")
@@ -10,6 +12,11 @@ public class EngineHouseApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EngineHouseApplication.class, args);
+	}
+	
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
 	}
 
 }
--- src/main/java/com/flow/service/ActiveProcessResource.java
+++ src/main/java/com/flow/service/ActiveProcessResource.java
@@ -1,5 +1,54 @@
 package com.flow.service;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import org.flowable.engine.RuntimeService;
+import org.flowable.engine.repository.ProcessDefinition;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.client.RestTemplate;
+import org.w3c.dom.Document;
+
+@RestController
+@RequestMapping("/processes")
 public class ActiveProcessResource {
 
+@Autowired private RestTemplate restTemplate;
+@Autowired RuntimeService runtimeService;
+@Autowired ProcessDefinition processDefinition;
+
+//Mapping that gets a process key from an external service and uses it to start an
+//instance of a process definition.
+
+
+@SuppressWarnings("rawtypes")
+@GetMapping("/{processName}")
+public MediaType.TEXT_XML_VALUE fetchProcessDocument(@PathVariable String processName){
+	
+	//Make rest call to service... flowProcess.getId needs switched.
+	ResponseEntity doc = restTemplate.getForEntity("http://localhost:8081/processes/"+processName, MediaType.TEXT_XML_VALUE.getClass());
+	
+	OutputStream output = new FileOutputStream("c:\\data\\output-text.txt");
+	Document data = (Document) doc.getBody();
+
+	while(doc.getStatusCode().value() <= 400) {
+	  int data = getMoreData();
+	  output.write(data);
+	}
+	output.close();
+	//take that information and use it to start an instance of that process
+	//perhaps save process to project root then dispose of it when finished	
+	
+	return MediaType.TEXT_XML_VALUE;
+}
+
+@PostMapping(path="/newProcess", consumes= "application/xml" , produces="aplication/xml")
+public void saveNewProcessDefinition() {
+	//This mapping will accept new xml over http and stream it into a file
+	//within the processes folder for future use.
+}
 }
