856acb3e23cbf7f1ca23c041160ff731c14ceb8b
wobblewedge <andobrown71@gmail.com>
2019-08-19 15:46:59 -0400

    Reverted to simplify

--- src/main/java/com/flow/enginehouse/controller/ApplicantController.java
+++ src/main/java/com/flow/enginehouse/controller/ApplicantController.java
@@ -8,14 +8,16 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.flow.enginehouse.util.*;
+import com.sun.mail.iap.Response;
 import com.flow.enginehouse.dto.ApplicantDto;
 import com.flow.enginehouse.entity.ApplicantRepository;
 import com.flow.enginehouse.service.AppService;
 
-@RequestMapping("/applicants")
+@RequestMapping(Constants.ROOT_API)
 @RestController
 public class ApplicantController {
 	@Autowired
@@ -23,17 +25,24 @@ public class ApplicantController {
 	@Autowired
 	ApplicantRepository applicantRepo;
 
-	@RequestMapping(Constants.GET_APPLICANT_BY_ID)
+	@RequestMapping(Constants.RETRIEVE_PROCESSES)
 	public ApplicantDto getUserById(@PathVariable Long id) {
 		return appService.getApplicantById(id);
 	}
+	
+	/*@RequestMapping("repository/process-defn=initions?key=loan-2-app.bpm20.xml")
+	public String getProcessDefinitions() {
+		ResponseBody response = new Response();
+		return 
+	}*/
+	
 
-	@RequestMapping(Constants.GET_ALL_USERS)
+	@RequestMapping("")
 	public List<ApplicantDto> getAllUsers() {
 		return appService.getAllUsers();
 	}
 
-	@RequestMapping(value = Constants.SAVE_USER, method = RequestMethod.POST)
+	@RequestMapping(value = Constants.SUBMIT_PROCESS, method = RequestMethod.POST)
 	public Map<String, Object> saveUser(@RequestBody ApplicantDto applicantDto) {
 		return appService.saveUser(applicantDto);
 	}
--- src/main/java/com/flow/enginehouse/util/Constants.java
+++ src/main/java/com/flow/enginehouse/util/Constants.java
@@ -1,7 +1,19 @@
 package com.flow.enginehouse.util;
 
 public interface Constants {
+	//Endpoints
 	static final String GET_APPLICANT_BY_ID = "/get/{id}";
 	static final String GET_ALL_USERS = "/getAll";
 	static final String SAVE_USER = "/save";
+	static final String ROOT_API = "/api/bpm/loanApproval";
+	static final String SUBMIT_PROCESS = "/process/startProcess";
+	static final String RETRIEVE_TAKS = "/task/retrieveTasks";
+	static final String RETRIEVE_PROCESSES = "/retrieveProcesses/{id}";
+	static final String COMPLETE_TASK = "/task/completeTask";
+	
+	//Loan Possibilities
+	static final String LOAN_APPROVED = "APPROVED";
+	static final String LOAN_REJECTED = "REJECTED";
+	static final String LOAN_PROGRESS = "IN_PROGRESS";
+	
 }
